584a0f8d0ed393b132b20befe066518efe833e7f
Swapnil Pore <pswapnilguru@gmail.com>
2021-08-16 11:37:37 +0200

    Database project added

---
+++ FavouriteAccounts/FavouriteAccount.Database/dbo/Tables/Bank.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Bank] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Code] NVARCHAR (4)  NOT NULL,
+    [Name] NVARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_Bank] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ FavouriteAccounts/FavouriteAccount.Database/dbo/Tables/Customer.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
