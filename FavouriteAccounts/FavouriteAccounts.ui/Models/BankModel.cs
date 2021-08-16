f89d1621cc45c6a146fe6feaffb0c98264e00678
Darek <dariusz.pelka@aerdata.com>
2021-08-16 12:04:45 +0000

    Add service classes

---
+++ FavouriteAccounts/FavouriteAccounts.ui/Models/BankModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FavouriteAccounts.ui.Models
+{
+    public class BankModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string IBAN { get; set; }
+    }
+}
