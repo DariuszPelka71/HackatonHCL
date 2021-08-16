8ac969eee3b0d6079aaf9af06d7bf15bf9b4f298
Karol Karpierz <k.karpierz.mail@gmail.com>
2021-08-16 10:12:18 +0000

    Prepare validation method

--- FavouriteAccounts/FavouriteAccounts.ui/Controllers/CustomerController.cs
+++ FavouriteAccounts/FavouriteAccounts.ui/Controllers/CustomerController.cs
@@ -16,25 +16,14 @@ namespace FavouriteAccounts.ui.Controllers
             return View();
         }
 
-        // GET: Customer/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Customer/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Customer/Create
+        // POST: Customer/Validate
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Validate(int CustomerId)
         {
             try
             {
                 // TODO: Add insert logic here
+                // call api with result ok for the login comparison
 
                 return RedirectToAction("Index");
             }
@@ -44,48 +33,5 @@ namespace FavouriteAccounts.ui.Controllers
             }
         }
 
-        // GET: Customer/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Customer/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Customer/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Customer/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
