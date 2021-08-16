c29ab6839fb65d0c70d403f611e93ebdd5314ff9
Darek <dariusz.pelka@aerdata.com>
2021-08-16 10:13:19 +0000

    Add fields to Create Favourite screen

---
+++ FavouriteAccounts/FavouriteAccounts.ui/Models/FavouriteAccountModel.cs
@@ -0,0 +1,16 @@
+namespace FavouriteAccounts.ui.Models
+{
+    public class FavouriteAccountModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int BankId { get; set; }
+    }
+
+}
