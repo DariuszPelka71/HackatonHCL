d8bc232666dd71601a8215e26f34d8b9664c1a66
Karol Karpierz <k.karpierz.mail@gmail.com>
2021-08-16 09:38:58 +0000

    Favourite Controller and CustomerController with Views added

---
+++ FavouriteAccounts/FavouriteAccounts.ui/Controllers/FavouriteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FavouriteAccounts.ui.Controllers
+{
+    public class FavouriteController : Controller
+    {
+        // GET: Favourite
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Favourite/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Favourite/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Favourite/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Favourite/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Favourite/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Favourite/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Favourite/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
